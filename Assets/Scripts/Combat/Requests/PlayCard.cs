079c3e5ad37f5ab886dd6b4bb60fcd074739ed0e
MnZn <2776835685@qq.com>
2023-03-29 17:44:47 +0800

    feature: 新增出牌请求

---
+++ Assets/Scripts/Combat/Requests/PlayCard.cs
@@ -0,0 +1,15 @@
+﻿using Combat.Cards;
+
+namespace Combat.Requests {
+// 出牌请求 Todo
+public class PlayCardRequest {
+    // 出牌者
+    public CombatantComponent Causer;
+
+    // 出牌目标(自己或对手)
+    public CombatantComponent Target;
+
+    // 打出的牌
+    public Card[] Cards;
+}
+}
