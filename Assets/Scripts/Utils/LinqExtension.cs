93847623800b4251a27a89d15ea78ce3fef34e1e
MnZn <2776835685@qq.com>
2023-03-29 09:18:43 +0800

    feature: 添加迭代器ForEach扩展

---
+++ Assets/Scripts/Utils/LinqExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils {
+public static class LinqExtension {
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+        foreach (var item in source) {
+            action(item);
+        }
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action) {
+        var i = 0;
+        foreach (var item in source) {
+            action(item, i);
+            i++;
+        }
+    }
+}
+}
