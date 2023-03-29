aa1db9e6c238c325224db3acfac92ef1cf1829d8
MnZn <2776835685@qq.com>
2023-03-29 09:22:50 +0800

    feature: 编写效果模板(组合式,临时式,触发式)

---
+++ Assets/Scripts/Combat/Effects/Templates/EffectTemporary.cs
@@ -0,0 +1,18 @@
+﻿namespace Combat.Effects.Templates {
+// 所有临时性增益/减益效果,失效前后不改变原有状态
+public class EffectTemporary : Effect {
+    public readonly CombatAddableState AddState = new();
+
+    protected override void OnAfterAttach() {
+        base.OnAfterAttach();
+
+        Target.State.Add(AddState);
+    }
+
+    protected override void OnLeaveAttach() {
+        base.OnLeaveAttach();
+
+        Target.State.Sub(AddState);
+    }
+}
+}
---
+++ Assets/Scripts/Combat/Effects/Templates/EffectTrigger.cs
@@ -0,0 +1,20 @@
+﻿namespace Combat.Effects.Templates {
+// 触发性buff
+public class EffectTrigger {
+    public int  MaxTriggerTimes = 1;
+    public bool InValid => MaxTriggerTimes <= 0;
+
+    private readonly Effect m_effect;
+
+    public EffectTrigger(Effect effect) {
+        m_effect = effect;
+    }
+
+    public bool Trigger(bool trigger = true) {
+        if (!trigger || InValid) return false;
+        --MaxTriggerTimes;
+        if (InValid) m_effect.Remove();
+        return true;
+    }
+}
+}
