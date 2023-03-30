38a17b7e5120a2a8f73f828baa904b50dd4a6015
MnZn <2776835685@qq.com>
2023-03-30 11:06:12 +0800

    feature: 添加一次性效果模板

---
+++ Assets/Scripts/Combat/Effects/Templates/EffectOnce.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Combat.Effects.Templates {
+// 一次性效果,只在附加时触发一次然后立刻移除
+public class EffectOnce : Effect {
+    public Action<EffectOnce> OnTrigger = delegate { };
+
+    public EffectOnce() {
+        UiHidde = true;
+    }
+
+    protected override void OnAfterAttach() {
+        base.OnAfterAttach();
+
+        OnTrigger(this);
+        Remove();
+    }
+}
+}
