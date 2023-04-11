3a93ff9b16d75c926cb93cc34493e1fab7a911ad
MnZn <2776835685@qq.com>
2023-04-12 02:27:56 +0800

    feature: 优化card架构

--- Assets/Scripts/Combat/Cards/Card.cs
+++ Assets/Scripts/Combat/Cards/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Combat.Requests;
+using Combat.Enums;
+using Combat.Requests.Details;
 
 namespace Combat.Cards {
 // 卡牌 基类
@@ -9,15 +10,24 @@ public class Card {
     // 卡牌持有者
     public CombatantComponent Owner;
 
+    // 卡牌是否被选中
+    public bool IsSelected = false;
+
+    // 卡牌是否可以被选中
+    public bool IsSelectable = true;
+
+    // 克隆原始卡牌
+    public Func<Card> Clone;
+
 #endregion
 
 #region UI配置
 
     // 卡牌名称
-    public string UiName = "Card";
+    public string UiName = "_None_";
 
     // 卡牌描述
-    public string UiDescription = "Card";
+    public string UiDescription = "_None_";
 
     // 卡牌图片路径 Todo 改成在编辑器里选材质
     public string UiImagePath = "";
@@ -27,7 +37,7 @@ public class Card {
 #region 逻辑配置
 
     // 法力消耗
-    public float LgManaCost = 0;
+    public float LgManaCost;
 
     // 动态法力消耗
     public Func<Card, float> LgManaCostFunc = null;
@@ -41,24 +51,51 @@ public class Card {
     // 卡牌本身的元素类型(仅用于触发元素联动,不一定造成伤害)
     public ElementType? LgElement = null;
 
+    // 卡牌是否有浸染效果(染色)
+    public bool LgInfect = false;
+
+    // 卡牌是否有唯一(不能搭配其他牌)效果 Todo 处理唯一效果
+    public bool LgUnique = false;
+
 #endregion
 
 #region 事件配置
 
+    // 前处理法力消耗,如元素浸染
+    public Action<RequestPlayBatchCard> OnBeforePreviewMana = delegate { };
+
+    // 后处理法力消耗,如本次法力消耗减半效果
+    public Action<RequestPlayBatchCard> OnAfterPreviewMana = delegate { };
+
+    // 计算元素联动前,如浸染效果
+    public Action<RequestPlayBatchCard> OnBeforePlayBatchCard = delegate { };
+
     // 卡牌效果
-    public Action<PlayCardRequest> OnPlay = delegate { };
+    public Action<RequestPlayCard> OnExecute = delegate { };
+
+    // 出牌后
+    public Action<RequestPlayBatchCard> OnAfterPlayBatchCard = delegate { };
 
 #endregion
 
 #region 公开函数
 
-    public float GetManaCost() {
-        return LgManaCostFunc?.Invoke(this) ?? LgManaCost;
+    public float ManaCost => LgManaCostFunc?.Invoke(this) ?? LgManaCost;
+
+    private float Damage => LgDamageFunc?.Invoke(this) ?? LgDamage;
+
+    public void ConfirmValue() {
+        LgManaCost = ManaCost;
+        LgDamage   = Damage;
     }
 
-    // 出牌的逻辑操作
-    public void PlayCard(PlayCardRequest request) {
-        OnPlay(request);
+    // 快捷造成伤害
+    public void TakeDamage(RequestPlayCard request) {
+        request.Causer.Attack(request.Batch.Target, new RequestHpChange {
+            Value   = Damage,
+            Type    = DamageType.Physical,
+            Element = LgElement,
+        });
     }
 
 #endregion
