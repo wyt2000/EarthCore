9b2ac549cec8e694cc9af1bfc30cdc8284ef4c88
MnZn <2776835685@qq.com>
2023-04-12 02:18:27 +0800

    feature: 新增玩家控制器

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Combat;
+using Combat.Requests.Details;
+using UnityEngine;
+
+namespace Controllers {
+public class PlayerController : MonoBehaviour {
+#region prefab配置
+
+    // 战斗对象
+    public CombatantComponent combatant;
+
+#endregion
+
+    // 用户输入
+    public void OnUserInput() {
+        // 摸牌
+        if (Input.GetKeyDown(KeyCode.S)) {
+            combatant.GetCard(1);
+        }
+        // 出牌
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var card = combatant.Cards.Where(c => c.IsSelected);
+            combatant.PlayCard(combatant.Opponent, new RequestPlayBatchCard {
+                Cards = card.ToArray()
+            });
+        }
+        // 弃牌
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            // Todo 
+        }
+    }
+}
+}
