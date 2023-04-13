e7143e0f66ce0770feb9f37f03af5eaf82c7488e
MnZn <2776835685@qq.com>
2023-04-13 20:28:45 +0800

    style: 优化ui

--- Assets/Scripts/GUIs/StateBarView.cs
+++ Assets/Scripts/GUIs/StateBarView.cs
@@ -19,10 +19,11 @@ public class StateBarView : MonoBehaviour {
     private void Update() {
         var state = combatant.State;
         // {combatant.name} status : 
-        var element = string.Join(",", state.ElementAttach.Keys.Select(v => v.ToDescription()));
-        var str = $"元素附着x{element}\n";
+        var element = string.Join(",", state.ElementAttach.Select(item => $"{item.Key.ToDescription()}x{item.Value}"));
+        var str = $"元素:{element}\n";
         str += $"生命值 : {state.Health:F0}/{state.HealthMax} \n";
         str += $"魔法值 : {state.Mana:F0}/{state.ManaMax} \n";
+        str += $"护甲 : {state.PhysicalArmor}\n";
         str += $"物理护盾 : {state.PhysicalShield}\n";
         str += $"物理伤害加成 : {state.PhysicalDamageAmplify}\n";
         str += $"物理伤害减免 : {state.PhysicalDamageReduce}\n";
