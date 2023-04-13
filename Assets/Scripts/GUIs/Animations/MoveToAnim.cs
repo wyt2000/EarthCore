0720bed55e6f4cc8cd90b7daf1b74b4262569f39
MnZn <2776835685@qq.com>
2023-04-13 16:49:08 +0800

    feature: 优化动画工具

--- Assets/Scripts/GUIs/Animations/MoveToAnim.cs
+++ Assets/Scripts/GUIs/Animations/MoveToAnim.cs
@@ -2,31 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Utils;
 
 namespace GUIs.Animations {
-// Todo 处理重复调用
 public static class MoveToAnim {
-    // private static readonly Dictionary<int, int> ms_refs = new();
-
-
     // 直线动画
-    public static IEnumerator MoveTo(this MonoBehaviour mono, Vector3 target, float duration) {
-        var id = mono.GetInstanceID();
-        var transform = mono.transform;
-        var startPosition = transform.position;
-        var startTime = Time.time;
-        var endTime = startTime + duration;
-        do {
-            var current = Time.time;
-            var t = Mathf.Clamp01((current - startTime) / duration);
-            transform.position = Vector3.Lerp(startPosition, target, t);
-            yield return null;
-        } while (Time.time < endTime);
-        transform.position = target;
+    public static IEnumerator MoveTo(this MonoBehaviour mono, AnimLocker locker, Vector3 target, float duration) {
+        var trans = mono.transform;
+        var start = trans.position;
+        return GAnimation.Lerp(locker, duration, t => trans.position = Vector3.Lerp(start, target, t));
     }
 
-    // 直线路径动画
-    public static IEnumerator MoveByPath(this MonoBehaviour mono, IEnumerable<Vector3> path, float duration) {
+    // 直线路径动画 Todo 改成无冲突的
+    public static IEnumerator MoveByPath(this MonoBehaviour mono, AnimLocker locker, IEnumerable<Vector3> path, float duration) {
         var nodes = path.ToArray();
         if (nodes.Length == 0) yield break;
         var transform = mono.transform;
@@ -36,7 +24,7 @@ public static class MoveToAnim {
         }
         var pre = transform.position;
         foreach (var node in nodes) {
-            yield return mono.MoveTo(node, Vector3.Distance(pre, node) / sum * duration);
+            yield return mono.MoveTo(locker, node, Vector3.Distance(pre, node) / sum * duration);
             pre = node;
         }
     }
---
+++ Assets/Scripts/GUIs/Animations/ValueToAnim.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using Utils;
+
+namespace GUIs.Animations {
+// 数值平滑动画(浮点属性用)
+public static class ValueToAnim {
+    public static IEnumerator SetTo(this TextMeshProUGUI text, AnimLocker locker, float old, float target, float duration) {
+        return GAnimation.Lerp(locker, duration, t => text.text = $"{Mathf.Lerp(old, target, t):F2}");
+    }
+}
+}
