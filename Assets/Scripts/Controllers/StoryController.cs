fd44eb270aa3c25b689b91625c7e319d7a197c4e
MnZn <2776835685@qq.com>
2023-04-24 00:37:52 +0800

    feature: 添加剧情演出控制器待办

---
+++ Assets/Scripts/Controllers/StoryController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace Controllers {
+// 剧情演出控制器 Todo 控制剧情演出流程
+public class StoryController : CombatController {
+    public override IEnumerator OnUserInput() {
+        yield break;
+    }
+}
+}
