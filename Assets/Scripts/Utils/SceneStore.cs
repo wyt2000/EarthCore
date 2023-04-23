63b7253387317cacff19fce0ba849aed3bdf9e31
MnZn <2776835685@qq.com>
2023-04-24 00:59:27 +0800

    feature: 添加跨场景传参容器

---
+++ Assets/Scripts/Utils/SceneStore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Utils {
+// 场景路由器
+public class SceneStore : MonoBehaviour {
+    private object m_data;
+
+    private static SceneStore ms_instance;
+
+    private void Start() {
+        if (ms_instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        ms_instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void Store(object data) {
+        ms_instance.m_data = data;
+    }
+
+    public static T Get<T>() {
+        return (T)ms_instance.m_data;
+    }
+}
+}
