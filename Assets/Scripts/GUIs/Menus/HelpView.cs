77ce4e3f4301476fe58d9f19cb57d83e9f0a170c
MnZn <2776835685@qq.com>
2023-04-27 11:11:43 +0800

    feature: 添加教程对话框

---
+++ Assets/Scripts/GUIs/Menus/HelpView.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GUIs.Menus {
+// Todo! 完善教程框
+public class HelpView : MonoBehaviour {
+#region prefab配置
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+#endregion
+
+    public void Show(string msg) {
+        text.text = msg;
+        gameObject.SetActive(true);
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+}
+}
